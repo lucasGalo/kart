5021b49f52ae73e463fc64b0e89b90b05abcf636
Lucas <galo_pc@outlook.com>
2020-02-15 17:41:47 -0300

    # 1 Inicio projeto..

---
+++ src/main/java/com/galo/andromeda/KartApplication.java
@@ -0,0 +1,14 @@
+package com.galo.andromeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KartApplication.class, args);
+    }
+
+
+}
